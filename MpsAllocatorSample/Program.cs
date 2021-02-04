b29bc6f062952c76a0a0a3dbbc031fe0bf34ef2f
Dimitris Gkanatsios <digkanat@microsoft.com>
2021-02-03 20:46:53 -0800

    updates per PR review

--- MpsAllocatorSample/Program.cs
+++ MpsAllocatorSample/Program.cs
@@ -203,23 +203,16 @@ namespace MpsAllocator
             string region = Console.ReadLine();
             req2.PreferredRegions = new List<string>() {region};
             req2.SessionId = Guid.NewGuid().ToString();
-            // InitialPlayers is the list of the initial players of the game
-            // they might or might not end up connecting to the game
+            // Initial list of players (potentially matchmade) allowed to connect to the game.
+            // This list is passed to the game server when requested (via GSDK) and can be used to validate players connecting to it.
             Console.WriteLine("Type initial players ID, Enter to confirm. Empty string when you're done");
             string id = Console.ReadLine();
-            while(true)
+            while(!string.IsNullOrEmpty(id))
             {
-                if (!string.IsNullOrEmpty(id))
-                {
-                    req2.InitialPlayers ??= new List<string>();
-                    req2.InitialPlayers.Add(id.Trim());
-                    Console.WriteLine($"player with ID {id} was recorded, type the next one. Empty string when you're done");
-                    id = Console.ReadLine();
-                }
-                else
-                {
-                    break;
-                }
+                req2.InitialPlayers ??= new List<string>();
+                req2.InitialPlayers.Add(id.Trim());
+                Console.WriteLine($"player with ID {id} was recorded, type the next one. Empty string when you're done");
+                id = Console.ReadLine();
             } 
             var res = await PlayFabMultiplayerAPI.RequestMultiplayerServerAsync(req2);
             if (res.Error != null)
