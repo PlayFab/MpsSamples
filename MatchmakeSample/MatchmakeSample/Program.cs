59c366bb89cb65d5b336c39e1167ce154c590afe
Paul Gilmore <pagilmor@microsoft.com>
2021-03-08 14:36:39 -0800

    New MatchmakeSample

---
+++ MatchmakeSample/MatchmakeSample/Program.cs
@@ -0,0 +1,178 @@
+﻿using PlayFab;
+using PlayFab.ClientModels;
+using PlayFab.MultiplayerModels;
+using System;
+using System.Collections.Generic;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace WindowsRunnerCSharpClient
+{
+    /// <summary>
+    ///   Simple executable that integrates with PlayFab's SDK.
+    ///   It allocates a game server and makes an http request to that game server
+    /// </summary>
+    public class Program
+    {
+        private static readonly PlayFabApiSettings settings = new PlayFabApiSettings();
+        private static readonly List<Player> players = new List<Player>();
+
+        public static Task Main(string[] args)
+        {
+            RootCommand rootCommand = RootCommandConfiguration.GenerateCommand(Run);
+
+            return rootCommand.InvokeAsync(args);
+        }
+
+        private static async Task Run(string titleId, int numPlayers, string mmQueueName)
+        {
+            settings.TitleId = titleId;
+
+            for (int i = 0; i < numPlayers; i++)
+            {
+                Player eachPlayer = new Player(Guid.NewGuid().ToString(), settings);
+                await Login(eachPlayer);
+                players.Add(eachPlayer);
+            }
+
+            VerifyNumPlayers(2, "matchmaking");
+            foreach (Player eachPlayer in players)
+            {
+                await CreateMatchmakeTicket(eachPlayer, mmQueueName);
+            }
+            bool success = true;
+            foreach (Player eachPlayer in players)
+            {
+                success &= await WaitForTicket(eachPlayer, mmQueueName);
+            }
+            VerifySuccess(success, "Matchmake-All Players");
+            foreach (Player eachPlayer in players)
+            {
+                await GetFinalMatch(eachPlayer, mmQueueName);
+            }
+        }
+
+        private static void VerifyNumPlayers(int numRequired, string feature)
+        {
+            if (players.Count < numRequired)
+            {
+                Console.WriteLine($"Need at least {numRequired} player(s) to test {feature}, playerCount: {players.Count}");
+                throw new Exception($"Need at least {numRequired} player(s) to test {feature}, playerCount: {players.Count}");
+            }
+        }
+
+        private static TResult VerifyPlayFabCall<TResult>(PlayFabResult<TResult> playFabResult, string throwMsg) where TResult : PlayFab.Internal.PlayFabResultCommon
+        {
+            if (playFabResult.Error != null)
+            {
+                Console.WriteLine(playFabResult.Error.GenerateErrorReport());
+                throw new Exception($"{throwMsg} HttpStatus={playFabResult.Error.HttpStatus}");
+            }
+            return playFabResult.Result;
+        }
+
+        private static void VerifySuccess(bool success, string eventName)
+        {
+            if (success)
+            {
+                Console.WriteLine($"{eventName} succeeded.");
+            }
+            else
+            {
+                Console.WriteLine($"{eventName} did not succeed.");
+                throw new Exception($"{eventName} did not succeed.");
+            }
+        }
+
+        private static async Task Login(Player player)
+        {
+            var loginRequest = new LoginWithCustomIDRequest()
+            {
+                CustomId = player.customId,
+                CreateAccount = true
+            };
+            PlayFabResult<LoginResult> login = await player.clientApi.LoginWithCustomIDAsync(loginRequest);
+            LoginResult loginResult = VerifyPlayFabCall(login, "Login failed");
+            Console.WriteLine($"Logged in player {login.Result.PlayFabId}, CustomId={loginRequest.CustomId}");
+        }
+
+        private static async Task CreateMatchmakeTicket(Player player, string mmQueueName)
+        {
+            var createRequest = new CreateMatchmakingTicketRequest
+            {
+                QueueName = mmQueueName,
+                GiveUpAfterSeconds = 15,
+                Creator = new MatchmakingPlayer { Entity = new PlayFab.MultiplayerModels.EntityKey { Id = player.context.EntityId, Type = player.context.EntityType } } // Why isn't this just the caller?
+            };
+            PlayFabResult<CreateMatchmakingTicketResult> ticketResult = await player.mpApi.CreateMatchmakingTicketAsync(createRequest);
+            CreateMatchmakingTicketResult ticket = VerifyPlayFabCall(ticketResult, "Failed to create matchmake ticket");
+            player.mmTicketId = ticket.TicketId;
+        }
+
+        private static async Task<bool> WaitForTicket(Player player, string mmQueueName)
+        {
+            var getTicketRequest = new GetMatchmakingTicketRequest
+            {
+                TicketId = player.mmTicketId,
+                QueueName = mmQueueName // Why isn't this discoverable from the ticketId?
+            };
+
+            bool success = false;
+            for (int i = 0; i < 10; i++)
+            {
+                PlayFabResult<GetMatchmakingTicketResult> ticketResult = await player.mpApi.GetMatchmakingTicketAsync(getTicketRequest);
+                GetMatchmakingTicketResult ticket = VerifyPlayFabCall(ticketResult, "Matchmake ticket poll failed.");
+                if (ticket.Status == "Matched")
+                {
+                    player.mmMatchId = ticket.MatchId;
+                    player.ticket = ticket;
+                    success = true;
+                    break;
+                }
+                System.Threading.Thread.Sleep(6000);
+                // "WaitingForMatch", "Matched"
+            }
+            VerifySuccess(success, $"Matchmake for {player.context.PlayFabId}");
+            return success;
+        }
+
+        private static async Task<GetMatchResult> GetFinalMatch(Player player, string mmQueueName)
+        {
+            var getRequest = new GetMatchRequest
+            {
+                QueueName = mmQueueName,
+                MatchId = player.mmMatchId
+            };
+            PlayFabResult<GetMatchResult> ticketResult = await player.mpApi.GetMatchAsync(getRequest);
+            GetMatchResult match = VerifyPlayFabCall(ticketResult, "Failed to get final matchmake ticket");
+            player.match = match;
+            Console.WriteLine($"{player.context.PlayFabId} matched: {match.MatchId}");
+            return match;
+        }
+    }
+
+    public class Player
+    {
+        public readonly string customId;
+        public readonly HttpClient httpClient;
+        public readonly PlayFabAuthenticationContext context;
+        public readonly PlayFabClientInstanceAPI clientApi;
+        public readonly PlayFabMultiplayerInstanceAPI mpApi;
+
+        public string mmTicketId;
+        public string mmMatchId;
+        public GetMatchmakingTicketResult ticket;
+        public GetMatchResult match;
+
+        public Player(string customId, PlayFabApiSettings settings)
+        {
+            this.customId = customId;
+            httpClient = new HttpClient();
+            context = new PlayFabAuthenticationContext();
+            clientApi = new PlayFabClientInstanceAPI(settings, context);
+            mpApi = new PlayFabMultiplayerInstanceAPI(settings, context);
+        }
+    }
+}
---
+++ MatchmakeSample/README.md
@@ -0,0 +1,38 @@
+# MatchmakeSample
+
+## Dependencies
+* .Net Core 3.1
+
+## Prerequisites
+* Understanding of C#
+* PlayFab Title with Matchmaking enabled
+
+## Overview
+MatchmakeSample is a sample executable that integrates with PlayFab's Matchmaking system. It logs in a configurable number of clients and attempts to matchmake them together.
+
+
+## Getting Started Guide
+
+### Running this sample locally
+1. Get your PlayFab TitleId and a Matchmake queue name for your title
+1. Build the solution, navigate to the binary output folder, and find the MatchmakeSample.dll output file
+1. Run the MatchmakeSample with the command line:
+    1. Depending on the number of players chosen and your queue settings, they may be matched into one or multiple groups
+
+```dotnet MatchmakeSample.dll --titleId <Your TitleId> --numPlayers 10 --mmQueueName <Name of Matchmake queue for your title>```
+
+With no parameters, this command will show these options:
+```
+Option '--titleId' is required.
+Option '--numPlayers' is required.
+Option '--mmQueueName' is required.
+
+Usage:
+  MatchmakeSample [options]
+
+Options:
+  --titleId <titleid>            Your PlayFab titleId (Hex)
+  --numPlayers <numplayers>      Number of players to create
+  --mmQueueName <mmqueuename>    Name of Matchmaking Queue to join
+  --version                      Display version information
+```
